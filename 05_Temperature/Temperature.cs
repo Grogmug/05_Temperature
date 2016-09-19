7a0232a34705cc52da64b3f8c6aad0ab2d8d0e6f
Søren <Sren@DESKTOP-3DFGV3A>
2016-09-19 11:21:50 +0200

    Add project files.

---
+++ 05_Temperature/Temperature.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Temperature
+{
+    enum Unit
+    {
+            Fahrenheit,
+            Celcius
+    }
+    class Temperature
+    {
+        public Temperature()
+        {
+
+        }
+        public Temperature(Unit type, double temp)
+        {
+            if (type == Unit.Celcius)
+            {
+                Celcius = temp;
+            }
+            else if (type == Unit.Fahrenheit)
+            {
+                Fahrenheit = temp;
+            }
+        }
+        public double Fahrenheit, Celcius;
+        public static double FahrenheitToCelcius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+
+        public static double CelciusToFahrenheit(double Celcius)
+        {
+            return Celcius * 9 / 5 + 32;
+        }
+
+
+    }
+}
---
+++ 05_Temperature/tempdone.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Temperature
+{/*
+    public enum Unit
+    {
+        Celcius,
+        Fahrenheit
+    } */
+    public class Tempdone
+    {
+
+        public double Fahrenheit, Celcius;
+        /*
+        public Tempdone(Unit type, double temp)
+        {
+            Fahrenheit = temp;
+            Celcius = temp;
+        } */
+
+        public Tempdone()
+        {
+           
+        }
+        public static double FahrenheitToCelcius(double fahrenheit)
+        {
+
+            return (fahrenheit - 32) * 5 / 9;
+        }
+
+        public static double CelciusToFahrenheit(double celcius)
+        {
+            return celcius * 9 / 5 + 32;
+        }
+
+
+    }
+}
